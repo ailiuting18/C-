e2ae1da83757dc29919390975053ebfc8c051726
ailiuting18 <107124811+ailiuting18@users.noreply.github.com>
2022-07-21 22:08:12 +0800

    Create coutest.cpp

---
+++ coutest.cpp
@@ -0,0 +1,13 @@
+// for ......
+#include <iostream>
+using namespace std;
+
+int main() {    
+    int number;
+
+    cout << "Enter an integer: ";
+    cin >> number;
+
+    cout << "You entered " << number;    
+    return 0;
+}
